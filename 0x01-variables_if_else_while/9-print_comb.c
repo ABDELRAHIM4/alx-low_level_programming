a66d9281ae89e55140414f7d8d25f27ca89ec71b
ABDELRAHIM <abdulrhimsaeed@gmail.com>
2023-09-10 07:05:17 -0700

    my commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
-int main (void) {
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print numbers from 0 to 9 separated by comma and space
+ * Return: Zero value
+ */
+
+int main(void)
+{
 	int n;
-	for (n= 48; n< 58; n++)
+
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
-		if (n != 57)
+		if (n != '9')
 		{
-			putchar('.');
+			putchar(',');
 			putchar(' ');
 		}
 	}
