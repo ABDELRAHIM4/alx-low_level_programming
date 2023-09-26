ca6b6e482d9e54801bae6153c968cf36958a1ed9
ABDELRAHIM <abdulrhimsaeed@gmail.com>
2023-09-26 13:03:05 -0700

    my commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,16 @@
 #include "main.h"
-
+/**
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ *
+ * PSEUDOCODE
+ * Loop through "s" using "n" as limit
+ *		replace s[i] with "b"
+ * return s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
