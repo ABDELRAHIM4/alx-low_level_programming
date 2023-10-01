f69207cc1287958a625d1a8fc818bca13b651d32
ABDELRAHIM <abdulrhimsaeed@gmail.com>
2023-09-30 18:04:19 -0700

    my commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,15 @@
-	#include "main.h"
-	/**
-	 *  * _puts_recursion - function like puts();
-	 *   * @s: input
-	 *    * Return: Always 0 (Success)
-	 *     */
+#include "main.h"
 
-	void _puts_recursion(char *s)
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
 	{
-			if (*s)
-					{
-								_putchar(*s);
-										_puts_recursion(s + 1);
-											}
-				else
-						{
-									_putchar('\n');
-										}
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
 }
 
