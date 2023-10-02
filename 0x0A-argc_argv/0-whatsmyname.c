4d0f193bcadada2d2894cbfd0284c6623b3b5efb
ABDELRAHIM <abdulrhimsaeed@gmail.com>
2023-10-02 15:08:56 -0700

    my commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- *  * main - prints program name, followed by a new line
- *   * @argc: int
- *    * @argv: list
- *     * Return : 0
+/**
+ *  * main - program that prints its name, followed by a new line
+ *   * @argc: number of arguments
+ *    * @argv: array with the arguments
+ *     *
+ *      * Return: always 0
 */
 int main(int argc, char const *argv[])
 {
