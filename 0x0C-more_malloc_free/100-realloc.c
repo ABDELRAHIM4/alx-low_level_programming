5f5ba2039d753d681233bc247650649a7551a20e
ABDELRAHIM <abdulrhimsaeed@gmail.com>
2023-10-09 14:35:50 -0700

    my commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 unsigned int i;
-char *p;
+void *p;
 if (new_size == old_size)
 return (NULL);
 if (new_size == 0 && ptr != NULL)
