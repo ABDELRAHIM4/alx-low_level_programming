0e642033efc5a6e06cd7560951a594154f277c6e
ABDELRAHIM <abdulrhimsaeed@gmail.com>
2023-10-10 16:10:53 -0700

    my commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  *  * main - Entry point
  *   *
  *    * Return: Always 0.
 */
+
 int main(void)
 {
-printf("%s \n", __FILE__);
+printf("%s\n", __FILE__);
 return (0);
 }
