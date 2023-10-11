4776708976b9247d6a8c57e3c4c6c956d5cb1c5a
ABDELRAHIM <abdulrhimsaeed@gmail.com>
2023-10-11 13:50:20 -0700

    my commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ *   * array_iterator - ...
+ *     * @array: ...
+ *       * @size: ...
+ *         * @action: ...
+ *           *
+ *             * Return: ...
+*/
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int *end = array + size - 1;
+if (array && size && action)
+while (array <= end)
+action(array++);
+}
+
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *   * int_index - ...
+ *     * @array: ...
+ *       * @size: ...
+ *         * @cmp: ...
+ *           *
+ *             * Return: ...
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i=0;
+if (array && size && cmp)
+while (i <= size)
+{
+if (cmp(array[i]))
+return (i);
+i++;
+}
+return (-1);
+}
