baa9edfdb7029d66d00931f969900fc9e89bf216
ABDELRAHIM <abdulrhimsaeed@gmail.com>
2023-10-12 09:44:00 -0700

    my commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ *   * sum_them_all - ...
+ *     * @n: ...
+ *       *
+ *         * Return: ...
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int x, s = 0;
+
+if (n == 0){
+return (0);
+}
+else 
+{
+va_list(p);
+va_start(p, n);
+for (x = 0; x < n; x++)
+s += va_arg(p, int);
+va_end(p);
+return s;
+}
+}
