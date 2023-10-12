d44639bfdad575c5efe937fd054de90b316edef7
ABDELRAHIM <abdulrhimsaeed@gmail.com>
2023-10-12 09:47:10 -0700

    my commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,16 +13,16 @@ int sum_them_all(const unsigned int n, ...)
 {
 unsigned int x, s = 0;
 
-if (n == 0){
+if(n == 0){
 return (0);
 }
-else 
+else
 {
 va_list(p);
 va_start(p, n);
 for (x = 0; x < n; x++)
 s += va_arg(p, int);
 va_end(p);
-return s;
+return (s);
 }
 }
