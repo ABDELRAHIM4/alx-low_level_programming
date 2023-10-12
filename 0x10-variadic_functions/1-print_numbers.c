3a0fc466e89d80ca396beaaaa07b8378ee5af1cb
ABDELRAHIM <abdulrhimsaeed@gmail.com>
2023-10-12 11:48:58 -0700

    my commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,11 +18,17 @@ if (separator != NULL)
 {
 va_list(p);
 va_start(p, n);
-for (x = 0; x < n * 3; x += 3)
+for (x = 0; x < n; x++)
 {
 vprintf("%d", p);
 if (x + 1 != n)
+{
 printf(",");
+}
+else
+{
+printf("\n");
+}
 }
 va_end(p);
 }
