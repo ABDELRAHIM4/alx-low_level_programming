5bafcb407ad7771142e5e64f5685429c035f12ec
Abdelrahim <abdulrhimsaeed@gmail.com>
2023-10-17 14:56:30 -0700

    my commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
-
 #include <stdlib.h>
 #include <stdio.h>
 /**
-* struct dog - this descripction about which dog
-* @name: sdfasdf
-* @age:  asdfas
-* @owner:  asdf
-*/
+ * struct dog - this descripction about which dog
+ * @name: sdfasdf
+ * @age:  asdfas
+ * @owner:  asdf
+ */
 
 struct dog
 {
-	char *n;
-	float a;
-	char *o;
+	char *name;
+	float age;
+	char *owner;
 };
 typedef struct dog dog_t;
-dog_t *new_dog(char *n, float a, char *o);
-void init_dog(struct dog *d, char *n, float a, char *o);
+dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
-
-#endif /* DOG_H */
+#endif /*DOG_H */
