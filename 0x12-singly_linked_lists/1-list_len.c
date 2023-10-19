fb8536d8032be8f707187079eec20b63563feb86
Abdelrahim <abdulrhimsaeed@gmail.com>
2023-10-19 14:00:49 -0700

    my commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t list_len(const list_t *h)
+{
+unsigned int n = 0;
+while(h)
+{
+n++;
+}
+return(n);
+
+}
