8ab6f4974718f6da27bb7eb32a5597e5714a61e7
Abdelrahim <abdulrhimsaeed@gmail.com>
2023-10-24 14:40:39 -0700

    my commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,16 +11,13 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-listint_t *ptr;
 unsigned int i = 0;
-if (head == NULL)
-return (NULL);
+listint_t *temp = head;
 
-while (i < index)
+while (temp && i < index)
 {
-ptr = head->next;
-head = ptr;
+temp = temp->next;
 i++;
 }
-return (ptr);
+return (temp ? temp : NULL);
 }
