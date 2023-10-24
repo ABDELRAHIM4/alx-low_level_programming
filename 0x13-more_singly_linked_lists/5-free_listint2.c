1997226c9f70e0cad4e20288a0cb3dac33d3d954
Abdelrahim <abdulrhimsaeed@gmail.com>
2023-10-24 09:47:05 -0700

    my commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+*/
+
+
+
+
+void free_listint2(listint_t **head)
+{
+listint_t *ptr;
+while (*head)
+{
+ptr = *head;
+ptr = ptr->next;
+*head = ptr;
+}
+free(*head);
+
+}
