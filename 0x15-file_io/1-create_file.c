a41ba69ef74207195559d5d182b9415c9013e569
Abdelrahim <abdulrhimsaeed@gmail.com>
2023-10-31 07:36:48 -0700

    my commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	output = write(fp, text_content, count);
+	out = write(fp, text_content, count);
 	if (out == -1 || out != count)
 		return (-1);
 
