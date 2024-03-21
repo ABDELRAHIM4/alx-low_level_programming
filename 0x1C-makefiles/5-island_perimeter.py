0f54a44e1bfe8200ee5aeaadfacd263ceee3997f
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-03-20 20:49:47 -0700

    make

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,14 +6,22 @@ def island_perimeter(grid):
     cols = len(grid[0])
     perimeter = 0
     for i in range(rows):
+        """iterate through each cell """
         for j in range(cols):
+            """iterate through each cell """
             if grid[i][j] == 1:
+                """if is land zone"""
                 if i == 0 or grid[i-1][j] == 0:
+                    """check the top"""
                     perimeter += 1
                 if i == rows-1 or grid[i+1][j] == 0:
+                    """check the bottom"""
                     perimeter += 1
                 if j == 0 or grid[i][j-1] == 0:
+                    """check the left"""
                     perimeter += 1
                 if j== cols-1 or grid[i][j+1] == 0:
+                    """check the right"""
                     perimeter += 1
+    """return perimeter"""
     return perimeter
